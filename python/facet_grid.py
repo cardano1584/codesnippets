ac6485a290cacb513fbca7c8ffdaddb85a439a90
David <avina.david.s@gmail.com>
2023-10-12 12:20:45 -0700

    Adding a pandas df for visualization

--- python/facet_grid.py
+++ python/facet_grid.py
@@ -1,11 +1,20 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-# Sample data
-tips = sns.load_dataset("tips")
+# Create a fictional DataFrame
+np.random.seed(42)
+data = {
+    'total_bill': np.random.uniform(10, 60, 200),
+    'tip': np.random.uniform(1, 12, 200),
+    'time': np.random.choice(['Lunch', 'Dinner'], 200),
+    'sex': np.random.choice(['Male', 'Female'], 200)
+}
+df = pd.DataFrame(data)
 
-# Create a FacetGrid
-g = sns.FacetGrid(tips, col="time", row="sex")
+# Use FacetGrid with the custom dataframe 'df'
+g = sns.FacetGrid(df, col="time", row="sex")
 g.map_dataframe(sns.scatterplot, x="total_bill", y="tip")
 g.set_axis_labels("Total bill ($)", "Tip ($)")
 g.set_titles(col_template="{col_name} patrons", row_template="{row_name}")
